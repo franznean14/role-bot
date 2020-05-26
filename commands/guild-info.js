076e4668b50f51f2d8d4c78faac1d8ce2df7f2d5
Franz Casabuena <franzneandra@gmail.com>
2020-05-26 19:09:43 +0800

    deleted unnecessary files

--- commands/guild-info.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-module.exports = {
-	name: 'guild-info',
-	description: 'Server Info!',
-	execute(message, args) {
-        message.channel.send(`Your server: ${message.guild.name}\nYour server ID: ${message.guild.id}`);
-	},
-};
--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-module.exports = {
-	name: 'ping',
-    description: 'Ping!',
-    cooldown: 5,
-	execute(message, args) {
-		message.channel.send('Pong.');
-	},
-};
